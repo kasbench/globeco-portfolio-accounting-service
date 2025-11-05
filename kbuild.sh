039caf338ace5f4da1c1861dffb960f09a50e535
Noah Krieger <noahkrieger@gmail.com>
2025-11-05 07:45:45 -0500

    froze docker tags

--- kbuild.sh
+++ kbuild.sh
@@ -1,6 +1,7 @@
 docker buildx build --platform linux/amd64,linux/arm64  \
 	--target production \
 	-t kasbench/globeco-portfolio-accounting-service-server:latest \
+	-t kasbench/globeco-portfolio-accounting-service-server:1.0.0 \
 	--push .
 kubectl delete -f k8s/portfolio-accounting-service-server.yaml
 kubectl apply -f k8s/portfolio-accounting-service-server.yaml
