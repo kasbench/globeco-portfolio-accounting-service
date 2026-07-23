afa666e41a34593895bc6c87265205b719e584ac
Noah Krieger <noahkrieger@gmail.com>
2026-07-22 20:56:16 -0400

    cleanup

--- cli_build.sh
+++ cli_build.sh
@@ -1,4 +1,5 @@
 docker buildx build --platform linux/amd64,linux/arm64  \
 	--target cli \
 	-t kasbench/globeco-portfolio-accounting-service-cli:latest \
+	-t kasbench/globeco-portfolio-accounting-service-cli:1.0.0 \
 	--push .
